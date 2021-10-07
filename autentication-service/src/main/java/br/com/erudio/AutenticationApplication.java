56ec82198ba37f8cc0ddca9404979ab2daa8bff4
Diego Freire <diego.freire@conductor.com.br>
2021-10-07 13:48:37 -0300

    Ajustes de autenticacao

--- autentication-service/src/main/java/br/com/erudio/AutenticationApplication.java
+++ autentication-service/src/main/java/br/com/erudio/AutenticationApplication.java
@@ -1,18 +1,15 @@
-package br.com.erudio.apigateway;
+package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class AutenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-		System.out.println("Link de acesso cambio: http://localhost:8765/cambio-service/8/USD/CLP");
-		System.out.println("Link de acesso cambio: http://localhost:8765/book-service/5/BRL");
+		SpringApplication.run(AutenticationApplication.class, args);
 		System.out.println("Link do Swagger: http://localhost:8765/webjars/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/");
 		System.out.println("Link do ZipKin para monitorar requests: http://localhost:9411/zipkin/?lookback=15m&endTs=1633460640845&limit=10");
-
 	}
 
 }
