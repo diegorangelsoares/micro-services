c2ba4cc1299cf6d725da7d30d87ae8f32a99eda1
Diego Freire <diego.freire@conductor.com.br>
2021-10-13 09:08:11 -0300

    Ajuste de autenticacao no service de cadastro

---
+++ cadastro-service/src/main/java/br/com/erudio/security/JWTConfiguracao.java
@@ -0,0 +1,38 @@
+package br.com.erudio.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+//                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAutenticarFilter(authenticationManager()))
+                .addFilter(new JWTValidarFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                //Liberar ts os endpoints sem altenticaçao
+                //.antMatchers(HttpMethod.GET, "/").permitAll();
+
+    }
+
+    //Liberar requisicao externa
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
